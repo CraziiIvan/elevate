e98ac84ae5c58f3065e9c839149b2a035fb132aa
Craziiivan <luckybn310@gmail.com>
2025-02-14 07:23:25 +0630

    update card ui

--- components/tool-card.tsx
+++ components/tool-card.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import { DiamondsFour } from "@phosphor-icons/react/dist/ssr";
+import { DiamondsFour, Hash, StarFour } from "@phosphor-icons/react/dist/ssr";
 import { getTagsByToolId } from "@/app/actions";
 import { cn } from "@/lib/utils";
+import Tag from "./ui/tag";
 
 type TToolCardProps = {
   toolId: number;
@@ -23,13 +24,14 @@ export default async function ToolCard({
   return (
     <div
       key={toolId}
-      className={cn("border-gray-3 relative space-y-3 rounded-2xl border p-4", {
+      className={cn("border-gray-2 relative space-y-3 rounded-2xl border p-4", {
         "p-5 shadow-[inset_0_0_20px_rgb(238,238,238,0.10)]": featured,
       })}
     >
       {featured && (
-        <div className="bg-gray-1 border-gray-3 absolute -top-2.5 right-4 flex h-6 items-center rounded-md border px-1.5 text-sm">
-          Featured
+        <div className="border-gray-3 absolute top-4 right-4 flex h-6 items-center gap-x-0.5 rounded-md border px-1 text-sm">
+          <StarFour size={12} weight="fill" />
+          <span className="px-0.5">Featured</span>
         </div>
       )}
       {logoUrl ? (
@@ -39,22 +41,20 @@ export default async function ToolCard({
           <DiamondsFour size={24} />
         </div>
       )}
-      <div className="space-y-2">
+      <div className="space-y-1.5">
         <div className="text-xl font-medium">{name}</div>
         <div className="text-gray-10 text-[15px]">{description}</div>
       </div>
-      {tags && tags.length > 0 && (
-        <div className="flex gap-x-1.5">
-          {tags.map((tag) => (
-            <div
-              key={tag.tagId}
-              className="bg-gray-2 text-gray-11 flex h-6 items-center rounded-md px-1.5 text-sm"
-            >
-              {tag.name}
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="flex items-center gap-x-1">
+        <Hash size={18} className="text-gray-6" />
+        {tags && tags.length > 0 && (
+          <>
+            {tags.map((tag) => (
+              <Tag key={tag.tagId} name={tag.name} />
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 }
